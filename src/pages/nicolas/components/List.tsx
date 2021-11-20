0cd826b8dc403297100512d3b6a706f5964ed11c
Niico <nicolas.gonz.mart@gmail.com>
2021-11-19 19:30:31 -0500

    cambio diseñÃo boton eliminar

--- src/pages/nicolas/components/List.tsx
+++ src/pages/nicolas/components/List.tsx
@@ -32,23 +32,32 @@ const ListPosts = ({ post, index, posts, setPosts }: Props): JSX.Element => {
           <div className="card-body text-light">
             <h4 className="card-title">{post.message}</h4>
             <h3 className="card-title">{post.date}</h3>
-              <div className="card-footer text-muted">
-                <div className="row">
+            <div className="card-footer text-muted">
+              <div className="row">
+                <div onClick={() => Likes(index)}>
+                  <i className="fas fa-thumbs-up btn btn-outline-secondary border-0"></i>
+                  <span>{post.likes}</span>
+                </div>
+                <div>
+              
 
-            <div onClick={() => Likes(index)} >
-              <i className="fas fa-thumbs-up btn btn-outline-secondary border-0"></i>
-              <span  >{post.likes}</span>
-            </div>
-            <div>
-              <button
-                className="btn btn-outline-secondary border-0 "
-                onClick={() => deletePost(index)}
-              >
-                <i className="fas fa-trash "></i>
-                <span>Eliminar post</span>
-              </button>
+                  <button
+                    onClick={() => deletePost(index)} 
+                    className="noselect">
+                    <span className="text">Delete</span>
+                    <span className="icon">
+                      <svg
+                        viewBox="0 0 24 24"
+                        height="24"
+                        width="24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+                      </svg>
+                    </span>
+                  </button>
+                </div>
               </div>
-            </div>
             </div>
           </div>
         </div>
--- src/pages/nicolas/components/PostForm.tsx
+++ src/pages/nicolas/components/PostForm.tsx
@@ -1,60 +1,67 @@
-import React , {useState}  from "react";
+import React, { useState } from "react";
 import { Post } from "../interface/PostNicolas";
 
 interface Props {
-    posts: Post[];
-    setPosts: any;
+  posts: Post[];
+  setPosts: any;
 }
 
-const PostForm = ({posts, setPosts}: Props): JSX.Element =>{
-    const [message, setMessage] = useState<string>("");
-    const [url,setUrl]= useState<string>("");
-    const date = new Date();
-    const today=`${date.getDate()} / ${date.getMonth()+1} / ${date.getFullYear()} ` 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) =>{
-        e.preventDefault();
-        Post(url,0,today,message);
-        setMessage("");
-        setUrl("")
-    };
+const PostForm = ({ posts, setPosts }: Props): JSX.Element => {
+  const [message, setMessage] = useState<string>("");
+  const [url, setUrl] = useState<string>("");
+  const date = new Date();
+  const today = `${date.getDate()} / ${
+    date.getMonth() + 1
+  } / ${date.getFullYear()} `;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    Post(url, 0, today, message);
+    setMessage("");
+    setUrl("");
+  };
 
-    const Post = (url: string, likes: number, date: string, message: string) =>{
-        const newList:Post[]= [...posts, {url,likes,date,message}]
-        setPosts(newList);
-    };
-    return (
-        <div className="container space">
-            <div className="card text-center  animate__animated animate__fadeInUp">
-                <div className="card-header" >
-                    Create a Post
-                </div>
-                <div className="card-body">
-                    <form onSubmit={handleSubmit}>
-                        <div className="mb-3">
-                            <input
-                                onChange={(e) => setMessage(e.target.value)}
-                                value={message}
-                                type="text"
-                                className="form-control"
-                                id="message"
-                                placeholder="Post mesagge."/>
-                        </div>
-                        <div className="mb-3">
-                            <input
-                                onChange={(e) => setUrl(e.target.value)}
-                                value={url}
-                                type="text"
-                                className="form-control"
-                                id="photoURL"
-                                placeholder="url."/>
-                        </div>
-                        <button type="submit" className="btn btn-info">subbmit</button>
-                    </form>
-                </div>
+  const Post = (url: string, likes: number, date: string, message: string) => {
+    const newList: Post[] = [...posts, { url, likes, date, message }];
+    setPosts(newList);
+  };
+  return (
+    <div className="container space">
+      <div className="card text-center  animate__animated animate__fadeInUp">
+        <div className="card-header">Create a Post</div>
+        <div className="card-body">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <input
+                onChange={(e) => setMessage(e.target.value)}
+                value={message}
+                type="text"
+                className="form-control"
+                id="message"
+                placeholder="Post mesagge."
+              />
             </div>
+            <div className="mb-3">
+              <input
+                onChange={(e) => setUrl(e.target.value)}
+                value={url}
+                type="text"
+                className="form-control"
+                id="photoURL"
+                placeholder="url."
+              />
+            </div>
+            <button className="cta">
+              <span>Submmit</span>
+              <svg width="15px" height="10px" viewBox="0 0 13 10">
+                <path d="M1,5 L11,5"></path>
+                <polyline points="8 1 12 5 8 9"></polyline>
+              </svg>
+            </button>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default PostForm
+export default PostForm;
